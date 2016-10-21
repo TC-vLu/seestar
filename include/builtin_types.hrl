c2791ab00854024b79e319da010423babc817163
David Liman <limanoit@gmail.com>
2016-10-21 16:08:28 -0700

    erlang 18 compatible

--- include/builtin_types.hrl
+++ include/builtin_types.hrl
@@ -1,9 +1,3 @@
--ifdef(only_builtin_types).
-    -type dict_t() :: dict().
-    -type queue_t() :: queue().
-    -type set_t() :: set().
--else.
-    -type dict_t() :: dict:dict().
-    -type queue_t() :: queue:queue().
-    -type set_t() :: set:set().
--endif.
+-type dict_t() :: dict:dict().
+-type queue_t() :: queue:queue().
+-type set_t() :: set:set().
